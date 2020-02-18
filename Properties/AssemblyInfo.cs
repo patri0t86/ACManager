b871bbd20d26cafa3d5869885bf9f86317ee1c1d
Alex Alford <alfordar@gmail.com>
2020-02-17 22:11:26 -0600

    v0.1.0.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,9 +7,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("ACManager")]
 [assembly: AssemblyDescription("Gameplay management plugin for Asheron's Call.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Alford Technologies, LLC")]
+[assembly: AssemblyCompany("ACManager")]
 [assembly: AssemblyProduct("ACManager")]
-[assembly: AssemblyCopyright("Alford Technologies, LLC")]
+[assembly: AssemblyCopyright("ACManager 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.4.67")]
-[assembly: AssemblyFileVersion("0.0.4.67")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
