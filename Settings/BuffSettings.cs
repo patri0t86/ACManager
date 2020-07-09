cb5fbd96e8861b285d5aac67b750fc9ff8d3f97d
Alex Alford <alfordar@gmail.com>
2020-07-08 23:17:53 -0400

    moved Utility to StateMachine namespace

---
+++ Settings/BuffSettings.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace ACManager.Settings
+{
+    [XmlRoot(ElementName = "Buffs")]
+    public class BuffSettings
+    {
+        public List<BuffProfile> BuffProfiles = new List<BuffProfile>();
+    }
+
+    public class BuffProfile
+    {
+        [XmlAttribute]
+        public string Command;
+
+        public List<Buff> Buffs = new List<Buff>();
+    }
+
+    public class Buff
+    {
+        [XmlAttribute]
+        public int SpellId;
+    }
+}
