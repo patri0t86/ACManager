3d3e051486ba81168379a239f85bc41b3bc08d89
Alex Alford <alfordar@gmail.com>
2020-07-21 11:57:36 -0400

    added banes only profile

---
+++ Settings/BuffDefaults/Banes.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace ACManager.Settings.BuffDefaults
+{
+    public class Banes
+    {
+        public List<string> Commands = new List<string>()
+            {
+                "banes",
+                "bane"
+            };
+        public List<Buff> Buffs = new List<Buff>();
+        public List<int> SpellList = new List<int>()
+            {
+                4407,
+                4412,
+                4393,
+                4397,
+                4401,
+                4403,
+                4409,
+                4391
+            };
+        public Banes()
+        {
+            foreach (int spellId in SpellList)
+            {
+                Buff buff = new Buff
+                {
+                    SpellId = spellId
+                };
+                Buffs.Add(buff);
+            }
+        }
+    }
+}
