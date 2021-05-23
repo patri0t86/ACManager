1e7eb8b331e55863f4c52a965e0bc59e1210b570
Alexander Alford <alfordar@gmail.com>
2021-05-22 22:35:02 -0400

    fixed request bug

--- StateMachine/Request.cs
+++ StateMachine/Request.cs
@@ -8,7 +8,7 @@ namespace ACManager.StateMachine
     public class Request : IEquatable<Request>
     {
         public RequestType RequestType;
-        public string RequesterName;
+        public string RequesterName = "";
         public int RequesterGuid;
         public string Character = CoreManager.Current.CharacterFilter.Name;
         public List<Spell> SpellsToCast = new List<Spell>();
